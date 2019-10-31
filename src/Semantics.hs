3f6500213656caf3b82f30fbe96190f558dcd9f3
Poscat <poscat@mail.poscat.moe>
2019-10-31 20:50:58 +0800

    Fix typo

--- src/Semantics.hs
+++ src/Semantics.hs
@@ -4,18 +4,18 @@ import           Lambda
 import           Control.Applicative            ( (<|>) )
 import qualified Parser                        as P
 
-subsitute :: Expr -> String -> Expr -> Expr
-subsitute (App exp1 exp2) s e = App (subsitute exp1 s e) (subsitute exp2 s e)
-subsitute (Lam s exp1) s' exp2 | s == s'   = error "Variable conflict"
-                               | otherwise = Lam s (subsitute exp1 s' exp2)
-subsitute (Var s) s' exp | s == s'   = exp
+substitute :: Expr -> String -> Expr -> Expr
+substitute (App exp1 exp2) s e = App (substitute exp1 s e) (substitute exp2 s e)
+substitute (Lam s exp1) s' exp2 | s == s'   = error "Variable naming conflict"
+                               | otherwise = Lam s (substitute exp1 s' exp2)
+substitute (Var s) s' exp | s == s'   = exp
                          | otherwise = Var s
 
 eval :: Expr -> Expr
-eval (Let s exp1 exp2) = eval $ subsitute exp2 s exp1
+eval (Let s exp1 exp2) = eval $ substitute exp2 s exp1
 eval (App exp1 exp2  ) = case exp1 of
-  Lam s exp1 -> eval $ subsitute exp1 s exp2
-  Var s      -> error "applying to a variable"
+  Lam s exp1 -> eval $ substitute exp1 s exp2
+  Var s      -> error "Applying a variable"
   App _ _    -> eval (App (eval exp1) exp2)
 eval x = x
 
