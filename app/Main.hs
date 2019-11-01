c9e0ad42a310b1a1719f1e894d8df9e5459e0cfe
Poscat <poscat@mail.poscat.moe>
2019-11-01 18:52:48 +0800

    Refine cli interface

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,21 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE BlockArguments #-}
 
 module Main where
 
 import           Parser
 import           Data.Char
+import           System.Console.Haskeline
+import           Control.Monad.Trans
 import qualified Lambda
 import qualified Semantics
 
 main :: IO ()
-main = do
-  putStr ">\n"
-  command <- getLine
-  putStrLn $ "result: " ++ show
-    (case runParser Lambda.expr command of
-      Just (e, _) -> Semantics.eval e
-      Nothing     -> error "Parse failed"
-    )
-  main
+main = runInputT defaultSettings loop
+  where loop = do
+            command <- getInputLine "> "
+            outputStrLn $ "result: " ++ show case command >>= \s -> runParser Lambda.expr s of
+                                      Just (e, _) -> Semantics.eval e
+                                      Nothing     -> error "Parse failed"
+                                    
+            loop
